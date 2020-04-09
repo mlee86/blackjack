0597e20e0f13d3386f7132e957a76a76b2cb89b1
Matt Lee <41455067+mlee86@users.noreply.github.com>
2020-04-09 16:08:09 -0400

    Update Person.js

--- Person.js
+++ Person.js
@@ -1,6 +1,25 @@
 class Person {
-  constructor(name) {
-    (this.name = name), (this.wins = 0), (this.losses = 0), (this.tie = 0);
+  name;
+  wins;
+  losses;
+  tie;
+  hand;
+  score;
+  money;
+  bet;
+  turn;
+  constructor(name){
+    this.name = name,
+    this.wins = 0,
+    this.losses = 0,
+    this.tie = 0,
+    this.hand = [],
+    this.score = 0,
+    this.money = 0
+    this.bet = 0;
+    this.turn = 0
   }
+
 }
+
 module.exports = Person;
