0994140ecb9367c7f2d9779bc66e17b5dd6dcd13
Matt Lee <41455067+mlee86@users.noreply.github.com>
2020-04-09 16:07:21 -0400

    Update blackJack.js

--- blackJack.js
+++ blackJack.js
@@ -7,25 +7,22 @@ const person = require("./Person");
 const players = require("./players.json");
 
 //variables
-let playerScore = 0;
-let dealerScore = 0;
-let playerCards = [];
-let dealerCards = [];
 let shoe = new deck();
-let player = ""; //new person
+let player = "";
+let dealer = new person("The Dealer")
 let hiddenCard = chalk.black.bgWhite("??");
 
 //Updates total score for both
 function updateScores() {
-  playerScore = getScore(playerCards);
-  dealerScore = getScore(dealerCards);
+  player.score = getScore(player.hand);
+  dealer.score = getScore(dealer.hand);
 }
 //Keeps half of dealer score hidden until player stops
 function updatedPlayerScore() {
-  playerScore = getScore(playerCards);
-  dealerScore = getCardValue(dealerCards[1]);
-  if (dealerScore === 1) {
-    dealerScore += 10;
+  player.score = getScore(player.hand);
+  dealer.score = getCardValue(dealer.hand[1]);
+  if (dealer.score === 1) {
+    dealer.score += 10;
   }
 }
 //Assigns value to each card
@@ -73,90 +70,114 @@ function getScore(cardArray) {
   }
   return score;
 }
-//Logs the results of the game and prompts to start a new game or exit
-function whoWins(player) {
-  if (playerScore > 21) {
-    console.log(`${player.name} has busted!`);
-    console.log("The Dealer has won");
-    player.losses++;
-    updatePlayer(player);
-    setTimeout(function () {
-      playAgain();
-    }, 2000);
-  } else if (dealerScore > 21) {
-    console.log("The Dealer has busted!");
-    console.log(`${player.name} has won`);
-    player.wins++;
-    updatePlayer(player);
-    setTimeout(function () {
-      playAgain();
-    }, 2000);
-  } else if (playerScore <= 21 && dealerScore < 17) {
-    return;
-  } else if (playerScore > dealerScore) {
-    console.log(`${player.name} has won`);
-    player.wins++;
-    updatePlayer(player);
-    setTimeout(function () {
-      playAgain();
-    }, 2000);
-  } else if (playerScore < dealerScore) {
-    if (dealerScore === 21) {
-      console.log("21! The Dealer has won");
-      player.losses++;
-      updatePlayer(player);
-      setTimeout(function () {
-        playAgain();
-      }, 2000);
-    } else {
-      console.log("The dealer has won.");
-      player.losses++;
-      updatePlayer(player);
-      setTimeout(function () {
-        playAgain();
-      }, 2000);
-    }
-  } else {
-    player.tie++;
-    updatePlayer(player);
-    console.log("It's a push! The game is a tie!");
-    setTimeout(function () {
-      playAgain();
-    }, 2000);
-  }
+
+function checkStats(){
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "return",
+        message: chalk.bgWhite.black(
+          `\n${player.name}:\nrecord: ${player.wins}-${player.losses}-${player.tie}\nMoney: $${player.money}\n`
+        ),
+        choices: ["BACK"],
+      },
+    ])
+    .then(function () {
+    whatToDo();
 }
-function playAgain(name) {
+)
+}
+
+function addMoney(){
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "money",
+        message: chalk.bgWhite.black(
+          `${player.name}:
+          Bank: ${player.money}
+          How much money would you like to add to your bank?`
+        ),
+        choices: ["$50", "$100", "$250", "$500", "$1000"],
+      },
+    ])
+    .then(function (response) {
+      money = parseInt(response.wager.replace('$', ''))
+        player.money += money
+        console.log(`$${money} added to account.
+        ${player.name} now has $${player.money}`);
+        whatToDo()
+})
+}
+
+function whatToDo(){
   inquirer
     .prompt([
       {
         type: "list",
         name: "action",
         message: chalk.bgWhite.black(
-          `\nGreat game ${player.name}\nWould you like to push your luck with another game?`
+          `\nWelcome ${player.name}\nWhat would you like to do?`
         ),
-        choices: ["Yes", "No", "Check Score"],
+        choices: ["Play Blackjack", "Check Stats", "Add Money", "Exit"],
       },
     ])
     .then(function (response) {
-      if (response.action === "Yes") {
-        console.log(chalk.bgRed.black(`Another round, ${player.name}!`));
-        startGame(name);
-      } else if (response.action === "Check Score") {
-        console.log(
-          `${player.name} has a record of: ${player.wins}-${player.losses}-${player.tie}`
-        );
-        setTimeout(function () {
-          playAgain();
-        }, 2500);
-      } else {
-        console.log(
-          chalk.bgRed.black(
-            `Thanks for playing ${player.name.toString("").toUpperCase()}`
-          )
-        );
+      if (response.action === "Play Blackjack") {
+        initialBet()
+      } 
+      if(response.action === "Check Stats"){
+        checkStats()
       }
-    });
+      if(response.action === "Add Money"){
+        addMoney()
+      }
+    })
 }
+//Logs the results of the game and prompts to start a new game or exit
+function whoWins(player) {
+  if (player.score > 21) {
+    console.log(`${player.name} has busted!`);
+    console.log("The Dealer has won");
+    player.losses++;
+    updatePlayer(player);
+  } else if (dealer.score > 21) {
+    console.log("The Dealer has busted!");
+    console.log(`${player.name} has won $${player.bet}`);
+    player.wins++;
+    player.money += (player.bet * 2)
+    updatePlayer(player);
+  } else if (player.score <= 21 && dealer.score < 17) {
+    return;
+  } else if (player.score > dealer.score) {
+    console.log(`${player.name} has won $${player.bet}`);
+    player.wins++;
+    player.money += (player.bet * 2)
+    updatePlayer(player);
+  } else if (player.score < dealer.score) {
+    if (dealer.score === 21) {
+      console.log("21! The Dealer has won");
+      player.losses++;
+      updatePlayer(player);
+    } else {
+      console.log("The dealer has won.");
+      player.losses++;
+      updatePlayer(player);
+    }
+  } else {
+    player.tie++;
+    player.money += player.bet
+    updatePlayer(player);
+    console.log("It's a push! The game is a tie!");
+    console.log(`${player.name} receives their original bet of $${player.bet} back`);
+  }
+  setTimeout(function(){
+    whatToDo()
+  }, 2000)
+}
+
 function dealCards(player) {
   player.push(shoe.deal());
 }
@@ -184,104 +205,162 @@ function getAllCards(cardArr) {
 }
 
 function playerAction() {
-  let lastCard = getCard(playerCards[playerCards.length - 1]);
+  let lastCard = getCard(player.hand[player.hand.length - 1]);
   console.log(`${player.name} was dealt ${lastCard}`);
   console.log(`${player.name} has: 
-          ${getAllCards(playerCards)}\n`);
+          ${getAllCards(player.hand)}\n`);
   console.log(`\nThe Dealer has: 
-          ${hiddenCard}  ${getCard(dealerCards[1])} \n`);
+          ${hiddenCard}  ${getCard(dealer.hand[1])} \n`);
   console.log(
-    `${player.name} now has ${playerScore} and the dealer has ${dealerScore}`
+    `${player.name} now has ${player.score} and the dealer has ${dealer.score}`
   );
+  console.log(`Current bet is: $${player.money}`)
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 }
 
 function dealerAction() {
-  let lastCard = getCard(dealerCards[dealerCards.length - 1]);
+  let lastCard = getCard(dealer.hand[dealer.hand.length - 1]);
   console.log(`The dealer was dealt: ${lastCard}`);
   console.log(`${player.name} has: 
-          ${getAllCards(playerCards)}\n`);
+          ${getAllCards(player.hand)}\n`);
   console.log(`\nThe Dealer has: 
-          ${getAllCards(dealerCards)}\n`);
+          ${getAllCards(dealer.hand)}\n`);
   console.log(
-    `${player.name} has ${playerScore} and the dealer has ${dealerScore}`
+    `${player.name} has ${player.score} and the dealer has ${dealer.score}`
   );
+  console.log(`Current bet is: $${player.money}`)
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 }
+
+function dealerTurn(){
+  updateScores();
+  console.log(`The Dealer has: 
+  ${getAllCards(dealer.hand)}
+  \nThe Dealer has ${dealer.score}`);
+  if (dealer.score < 17) {
+    var interval = setInterval(dealerHit, 1500);
+    function dealerHit() {
+      if (dealer.score >= 17) {
+        clearInterval(interval);
+      } else {
+        dealCards(dealer.hand), updateScores(), dealerAction();
+        whoWins(player);
+      }
+    }
+  } else {
+    setTimeout(function () {
+      whoWins(player);
+    }, 1500);
+  }
+}
+
+function ask() {
+  let options = []
+  if(player.turn > 1){
+    options = ["Hit", "Stay"]
+  } else {
+    options = ["Hit", "Stay", "Double Down"]
+  }
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: chalk.bgWhite.black(
+          `\n${player.name} currently has ${player.score} \nThe Dealer currently has ${dealer.score}\nCurrent bet: ${player.bet}\nWould you like to hit or stay?`
+        ),
+        choices: options,
+      },
+    ])
+    .then(function (response) {
+      if (response.action === "Hit") {
+        dealCards(player.hand);
+        updatedPlayerScore();
+        playerAction();
+        if (player.score <= 21) {
+          setTimeout(function () {
+            player.turn++
+            ask();
+          }, 1500);
+        } else {
+          setTimeout(function () {
+            whoWins(player);
+          }, 1500);
+        }
+      } else if (response.action === "Double Down"){
+        if(player.money < player.bet){
+          console.log("You do not have enough money to double down, please select another option");
+          ask();
+        } else {
+          player.money -= player.bet;
+          player.bet *= 2;
+          dealCards(player.hand);
+          updatedPlayerScore();
+          playerAction();
+          dealerTurn();
+        }
+      }
+      else {
+       dealerTurn()
+      }
+    });
+}
+function initialBet(){
+  player.bet = 0;
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "wager",
+        message: chalk.bgWhite.black(
+          `\n${player.name}:\nBank: $${player.money}\nPlease make an inital bet to play`
+        ),
+        choices: ["$5", "$10", "$15", "$20", "$50"],
+      },
+    ])
+    .then(function (response) {
+      wager = parseInt(response.wager.replace('$', ''))
+      if (wager > player.money) {
+        console.log("You do not have that much money available, please select a smaller bet");
+        initialBet();
+      } else{
+        player.money -= wager
+        player.bet = wager;
+        startGame();
+      }
+})
+}
+
 /*
 Initializes the actual game. This will set both hands to empty, create a deck and deal random cards
 The function will also prompt for user to hit or stay
 */
 function startGame() {
-  playerCards = [];
-  dealerCards = [];
+  player.hand = [];
+  dealer.hand = [];
+  player.turn = 0
   if (shoe.deck.length <= 26) {
     console.log("Time to shuffle the deck");
     shoe.reset();
     console.log(getAllCards(shoe.deck));
     shoe.shuffle();
   }
-  dealCards(playerCards);
-  dealCards(dealerCards);
-  dealCards(playerCards);
-  dealCards(dealerCards);
+  dealCards(player.hand);
+  dealCards(dealer.hand);
+  dealCards(player.hand);
+  dealCards(dealer.hand);
   updatedPlayerScore();
   setTimeout(function () {
+    player.turn++;
     console.log(`${player.name} has been dealt: 
-    ${getAllCards(playerCards)}\n${player.name} has ${playerScore}\n`);
+    ${getAllCards(player.hand)}\n${player.name} has ${player.score}\n`);
   }, 500);
   setTimeout(function () {
     console.log(`\nThe Dealer has been dealt: 
-    ${hiddenCard}  ${getCard(dealerCards[1])} \nThe Dealer has ${dealerScore}`);
-    ask(player.name);
+    ${hiddenCard}  ${getCard(dealer.hand[1])} \nThe Dealer has ${dealer.score}`);
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    ask();
   }, 1500);
-  function ask() {
-    inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "action",
-          message: chalk.bgWhite.black(
-            `\n${player.name} currently has ${playerScore} \nWould you like to hit or stay?`
-          ),
-          choices: ["Hit", "Stay"],
-        },
-      ])
-      .then(function (response) {
-        if (response.action === "Hit") {
-          dealCards(playerCards);
-          updatedPlayerScore();
-          playerAction();
-          if (playerScore <= 21) {
-            setTimeout(function () {
-              ask();
-            }, 1500);
-          } else {
-            setTimeout(function () {
-              whoWins(player);
-            }, 1500);
-          }
-        } else {
-          updateScores();
-          console.log(`The Dealer has: 
-          ${getAllCards(dealerCards)}
-          \nThe Dealer has ${dealerScore}`);
-          if (dealerScore < 17) {
-            var interval = setInterval(dealerHit, 1500);
-            function dealerHit() {
-              if (dealerScore >= 17) {
-                clearInterval(interval);
-              } else {
-                dealCards(dealerCards), updateScores(), dealerAction();
-                whoWins(player);
-              }
-            }
-          } else {
-            setTimeout(function () {
-              whoWins(player);
-            }, 1500);
-          }
-        }
-      });
-  }
 }
 //First prompt to get the players name and start the gameplay
 function getName() {
@@ -307,17 +386,16 @@ function getName() {
         player = new person(p.name);
         playerKey = player.name.replace(/\s+/g, "").toUpperCase();
         players[playerKey] = player;
-        fs.writeFileSync(
-          "./players.json",
-          JSON.stringify(players),
-          function () {
-            console.log("name added:");
-          }
-        );
+        updatePlayer(player);
       } else {
         player = players[key];
       }
-      startGame();
+      // startGame();
+      if(player.money < 10){
+        player.money = 100
+      }
+      whatToDo()
+      
     });
 }
 
